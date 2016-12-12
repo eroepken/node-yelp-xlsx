cb3583227dd57627a2167cc6cf0e696cd6673e04
Ellie Roepken <ellie.roepken@gmail.com>
2016-12-11 21:06:09 -0500

    Convert config to a separate json file and add requirement for array unique library.

--- index.js
+++ index.js
@@ -3,70 +3,76 @@
 const yelp = require('yelp-fusion');
 const json2xls = require('json2xls');
 const fs = require('fs');
+const unique = require('array-unique');
 
-// Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
-const clientId = 'REDACTED';
-const clientSecret = 'REDACTED';
+const config = require('./config.json');
 
 let numMiles = 7;
 const numMetersInMile = 1609.344;
 
-// Search up to 7 miles (11265.4 meters).
-const searchRequest = {
-  latitude: 40.920690,
-  longitude: -72.723794,
-  radius: parseInt(numMiles * numMetersInMile),
-  price: '1,2,3,4',
-  limit: 50
-};
+let allBusinesses = [];
 
-yelp.accessToken(clientId, clientSecret).then(response => {
-  let client = yelp.client(response.jsonBody.access_token);
+function yelpCall(searchParams) {
+  yelp.accessToken(config.appID, config.appSecret).then(response => {
+    let client = yelp.client(response.jsonBody.access_token);
 
-  if (typeof client !== 'undefined') {
-    console.log('Token obtained.');
-  }
-
-  client.search(searchRequest).then(response => {
-
-    let businesses = response.jsonBody.businesses;
-
-    for (var i = 0; i < businesses.length; i++) {
-      // Convert latitude and longitude into separate fields.
-      businesses[i].latitude = businesses[i].coordinates.latitude;
-      businesses[i].longitude = businesses[i].coordinates.longitude;
-      delete businesses[i].coordinates;
-
-      // Parse categories into separate fields.
-      if (businesses[i].categories.length > 0) {
-        for(var cat = 0; cat < businesses[i].categories.length; cat++) {
-          businesses[i]['category' + (cat+1)] = businesses[i].categories[cat].title;
-        }
-
-        delete businesses[i].categories;
-      }
-
-      // Convert the location field into a human readable address.
-      let street = businesses[i].location.address1;
-
-      if (businesses[i].location.address2 != '' && businesses[i].location.address2 != null) {
-        street += ' ' + businesses[i].location.address2;
-      }
-      if (businesses[i].location.address3 != '' && businesses[i].location.address3 != null) {
-        street += ' ' + businesses[i].location.address3;
-      }
-
-      let address =  street + ', ' + businesses[i].location.city + ', ' + businesses[i].location.state + ' ' + businesses[i].location.zip_code;
-      businesses[i].location = address;
+    if (typeof client !== 'undefined') {
+      console.log('Token obtained.');
     }
 
-    let xls = json2xls(businesses);
-    fs.writeFileSync('dataLat' + searchRequest.latitude + 'Long' + searchRequest.longitude + '.xlsx', xls, 'binary');
-    console.log('File created.');
+    let searchRequest = {
+      latitude: 40.920690,
+      longitude: -72.723794,
+      radius: parseInt(numMiles * numMetersInMile),
+      price: '1,2,3,4',
+      limit: 50
+    };
+
+    client.search(searchRequest).then(response => {
+
+      let businesses = response.jsonBody.businesses;
+
+      // We have to clean up the data that comes back.
+      for (let i = 0; i < businesses.length; i++) {
+        let bus = businesses[i];
+
+        // Convert latitude and longitude into separate fields.
+        bus.latitude = bus.coordinates.latitude;
+        bus.longitude = bus.coordinates.longitude;
+        delete bus.coordinates;
+
+        // Parse categories into separate fields.
+        if (bus.categories.length > 0) {
+          for(let cat = 0; cat < bus.categories.length; cat++) {
+            bus['category' + (cat+1)] = bus.categories[cat].title;
+          }
+
+          delete bus.categories;
+        }
+
+        // Convert the location field into a human readable address.
+        let street = bus.location.address1;
+
+        if (bus.location.address2 != '' && bus.location.address2 != null) {
+          street += ' ' + bus.location.address2;
+        }
+        if (bus.location.address3 != '' && bus.location.address3 != null) {
+          street += ' ' + bus.location.address3;
+        }
+
+        let address =  street + ', ' + bus.location.city + ', ' + bus.location.state + ' ' + bus.location.zip_code;
+        bus.location = address;
+
+        allBusinesses.push(bus);
+      }
+
+      // let xls = json2xls(businesses);
+      // fs.writeFileSync('data.xlsx', xls, 'binary');
+      console.log('File created.');
+    }).catch(e => {
+      console.log(e);
+    });
   }).catch(e => {
     console.log(e);
   });
-}).catch(e => {
-  console.log(e);
-});
+}
