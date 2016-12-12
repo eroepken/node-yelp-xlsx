0178fde04f61723be6928d7aae30b3b2fe685282
Ellie Strejlau <ellie.strejlau@gmail.com>
2016-12-12 00:57:28 -0500

    Ugh.

--- index.js
+++ index.js
@@ -7,72 +7,101 @@ const unique = require('array-unique');
 
 const config = require('./config.json');
 
-let numMiles = 7;
 const numMetersInMile = 1609.344;
+const limit = 50;
+const offset = 0;
 
-let allBusinesses = [];
+// Only get the access token once.
+yelp.accessToken(config.appID, config.appSecret).then(response => {
 
-function yelpCall(searchParams) {
-  yelp.accessToken(config.appID, config.appSecret).then(response => {
-    let client = yelp.client(response.jsonBody.access_token);
+  let client = yelp.client(response.jsonBody.access_token);
+  if (typeof client !== 'undefined') {
+    console.log('Token obtained.');
+  } else {
+    return FALSE;
+  }
 
-    if (typeof client !== 'undefined') {
-      console.log('Token obtained.');
+  let allBusinesses = [];
+
+  return getAllBusinesses(client).then(allBusinesses => {
+    allBusinesses.concat(businesses);
+    return allBusinesses;
+  });
+}).then(allBusinesses => {
+  console.log(allBusinesses);
+  // let xls = json2xls(unique(yelpToXLSX.allBusinesses));
+  // fs.writeFileSync('data.xlsx', xls, 'binary');
+  // console.log('File created.');
+}).catch(e => {
+  console.log(e);
+});
+
+function getAllBusinesses(client) {
+  // Cycle through all location objects.
+  return Promise.all(config.locations.map(function (row) {
+    let location = row;
+    if (typeof location.latitude === 'undefined' || typeof location.longitude === 'undefined') {
+      console.log('Lat/Long missing.');
+      return FALSE;
     }
 
     let searchRequest = {
-      latitude: 40.920690,
-      longitude: -72.723794,
-      radius: parseInt(numMiles * numMetersInMile),
-      price: '1,2,3,4',
-      limit: 50
+      latitude: location.latitude,
+      longitude: location.longitude,
+      radius: parseInt(config.numMiles * numMetersInMile),
+      price: config.price,
+      limit: limit,
+      offset: offset
     };
 
-    client.search(searchRequest).then(response => {
-
-      let businesses = response.jsonBody.businesses;
+    return client.search(searchRequest).then(response => {
+      let businesses = [];
 
       // We have to clean up the data that comes back.
-      for (let i = 0; i < businesses.length; i++) {
-        let bus = businesses[i];
-
-        // Convert latitude and longitude into separate fields.
-        bus.latitude = bus.coordinates.latitude;
-        bus.longitude = bus.coordinates.longitude;
-        delete bus.coordinates;
-
-        // Parse categories into separate fields.
-        if (bus.categories.length > 0) {
-          for(let cat = 0; cat < bus.categories.length; cat++) {
-            bus['category' + (cat+1)] = bus.categories[cat].title;
-          }
-
-          delete bus.categories;
-        }
-
-        // Convert the location field into a human readable address.
-        let street = bus.location.address1;
-
-        if (bus.location.address2 != '' && bus.location.address2 != null) {
-          street += ' ' + bus.location.address2;
-        }
-        if (bus.location.address3 != '' && bus.location.address3 != null) {
-          street += ' ' + bus.location.address3;
-        }
-
-        let address =  street + ', ' + bus.location.city + ', ' + bus.location.state + ' ' + bus.location.zip_code;
-        bus.location = address;
-
-        allBusinesses.push(bus);
+      for (let i = 0; i < response.jsonBody.businesses.length; i++) {
+        let bus = cleanResult(response.jsonBody.businesses[i]);
+        businesses.push(bus);
       }
 
-      // let xls = json2xls(businesses);
-      // fs.writeFileSync('data.xlsx', xls, 'binary');
-      console.log('File created.');
+      return businesses;
     }).catch(e => {
       console.log(e);
     });
-  }).catch(e => {
-    console.log(e);
-  });
+  }));
+}
+
+function cleanResult(bus) {
+  // Convert latitude and longitude into separate fields.
+  bus.latitude = bus.coordinates.latitude;
+  bus.longitude = bus.coordinates.longitude;
+  delete bus.coordinates;
+
+  // Parse categories into separate fields.
+  if (bus.categories.length > 0) {
+    for(let cat = 0; cat < bus.categories.length; cat++) {
+      bus['category' + (cat+1)] = bus.categories[cat].title;
+    }
+    delete bus.categories;
+  }
+
+  // Convert the location field into a human readable address.
+  let street = bus.location.address1;
+  if (bus.location.address2 != '' && bus.location.address2 != null) {
+    street += ' ' + bus.location.address2;
+  }
+
+  if (bus.location.address3 != '' && bus.location.address3 != null) {
+    street += ' ' + bus.location.address3;
+  }
+
+  let address = street + ', ' + bus.location.city + ', ' + bus.location.state + ' ' + bus.location.zip_code;
+  bus.location = address;
+
+  // Convert the business price to a number rather than dollar signs.
+  bus.price = bus.price.length;
+
+  // Remove the image url because we really don't need it.
+  delete bus.image_url;
+
+  return bus;
 }
